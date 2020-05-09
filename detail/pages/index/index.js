06039e3e0af1201745c0cfb9a5ad6b3115915cf2
xtx <2115435325@qq.com>
2020-05-09 21:14:32 +0800

    首页+详情

---
+++ detail/pages/index/index.js
@@ -0,0 +1,59 @@
+// index/index.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      {
+        link: '/pages/index/index',
+        url: '/image/1.jpg'
+      }, {
+        link: '/pages/index/index',
+        url: '/image/1.jpg'
+      }, {
+        link: '/pages/index/index',
+        url: '/image/1.jpg'
+      }
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    page:1
+
+    
+  },
+
+  immeBuy:function() {
+    wx.showToast({
+      title: '已标记商品！请联系卖家进行交易^_^',
+      icon:'success',
+      duration:2000
+    })
+  },
+
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    var href = this.data.imghref;
+    var goodsimg = this.data.goods_img;
+    var imglist = [];
+    for (var i = 0; i < goodsimg.length; i++) {
+      imglist[i] = href + goodsimg[i].img
+    }
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: imglist// 需要预览的图片http链接列表  
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let app = getApp()
+    this.getList()
+  },
+})
