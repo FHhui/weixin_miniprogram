9e7a1ef917da2403b67daf778e7e9e55f5fc6cac
FHhui <1013309386@qq.com>
2020-05-13 09:25:48 +0800

    集合版
    这是一个集合版，但是app.json我没有配置需要自己配

---
+++ miniprogram-4/pages/after_search_info/after_search_info.js
@@ -0,0 +1,131 @@
+// pages/after_search_info/after_search_info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+
+  data: {
+    dataList: [
+      {
+
+        goods_id: 1,
+
+        goods_title: '商品标题1',
+
+        goods_img: '/image/song.jpg',
+
+        goods_xiaoliang: '0',
+
+        goods_price: '60'
+
+      }, {
+
+        goods_id: 1,
+
+        goods_title: '商品标题2',
+
+        goods_img: '/image/song.jpg',
+
+        goods_xiaoliang: '0',
+
+        goods_price: '70'
+
+      }, {
+
+        goods_id: 1,
+
+        goods_title: '商品标题3',
+
+        goods_img: '/image/song.jpg',
+
+        goods_xiaoliang: '0',
+
+        goods_price: '80'
+
+      }, {
+
+        goods_id: 1,
+
+        goods_title: '商品标题4',
+
+        goods_img: '/image/song.jpg',
+
+        goods_xiaoliang: '0',
+
+        goods_price: '90'
+
+      }, {
+
+        goods_id: 1,
+
+        goods_title: '商品标题5',
+
+        goods_img: '/image/song.jpg',
+
+        goods_xiaoliang: '0',
+
+        goods_price: '110'
+
+      }
+
+    ],
+    searchinfo: "请输入搜索信息",
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ miniprogram-4/pages/record/record.js
@@ -0,0 +1,83 @@
+// pages/record/record.js
+//获取应用实例
+const app = getApp()
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    currentSelect: 1,
+    items: [{
+      'image': "/images/pot.jpg",
+      'name': '锅',
+      'price': '998.00',
+      'title': '一锅在手，天下我有。',
+      'type': 'true'
+    },
+    {
+      'image': "/images/bowl.jpg",
+      'name': '碗',
+      'price': '188.00',
+      'title': '天大地大，吃饭最大。',
+      'type': 'false'
+
+    },
+    {
+      'image': "/images/spoon.jpg",
+      'name': '瓢',
+      'price': '2.00',
+      'title': '想不出来，自己体会。',
+      'type': 'false'
+    }
+
+    ]
+  },
+  onLoad: function () {
+    var that = this;
+
+    /** 
+     * 获取系统信息 
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    }),
+      wx.setNavigationBarTitle({
+        title: '购物车',
+      })
+
+  },
+
+  /** 
+     * 滑动切换tab 
+     */
+  bindChange: function (e) {
+
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
+  /** 
+   * 点击tab切换 
+   */
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  }
+})
